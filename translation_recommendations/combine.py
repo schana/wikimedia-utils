0ceafc90d3abff98a2beee3ce5d9c1a1345387e7
Nathaniel Schaaf <nschaaf@wikimedia.org>
2017-06-30 19:19:00 +0200

    Remove file extension for column name

--- translation_recommendations/combine.py
+++ translation_recommendations/combine.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import sys
 
-merged = pd.read_csv(sys.argv[1], names=('id', sys.argv[1]))
+merged = pd.read_csv(sys.argv[1], names=('id', sys.argv[1].split('.')[0]))
 
 for wiki in sys.argv[2:]:
     print wiki
-    next_wiki = pd.read_csv(wiki, names=('id', wiki))
+    next_wiki = pd.read_csv(wiki, names=('id', wiki.split('.')[0]))
     merged = pd.merge(merged, next_wiki, on='id', how='outer')
 
 print 'merging'
